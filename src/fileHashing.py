26a00cafbaeb40b24ceb84b2005f43734976cb45
P S <internetz@eclipso.de>
2017-12-08 21:24:49 +0100

    uploaded fileHashing.py

---
+++ src/fileHashing.py
@@ -0,0 +1,77 @@
+import hashlib
+import os
+import platform
+import json
+
+#generates any checksum of a file
+def genFileChecksum(filename, algorythm='sha1', printing=False):
+    if algorythm == "sha256":
+        hasher = hashlib.sha256()
+    elif algorythm == "sha512":
+        hasher = hashlib.sha512()
+    elif algorythm == "sha1":
+        hasher = hashlib.sha1()
+    elif algorythm == "md5":
+        hasher = hashlib.md5()
+    else:
+        e = "CustomError: Algorythm", algorythm, 'is not supported. Sha1 will be used. Supported algorythms are "sha1", "sha256" and "sha512".'
+        print('ERROR fileHandler.py genFileChecksum(filename,algorythm)\n      ', e)
+        hasher = hashlib.sha1()
+    try:
+        try:
+            with open(filename, 'rb') as afile:
+                buf = afile.read(65536)
+                while len(buf) > 0:
+                    hasher.update(buf)
+                    buf = afile.read(65536)
+            checksum = hasher.hexdigest()
+            if not printing == False:
+                print(filename+" - "+checksum)
+            return checksum
+        except PermissionError:
+            return "ERROR"
+    except Exception as e:
+        print('ERROR fileHandler.py genFileChecksum(filename,algorythm)\n      ', e)
+        #print(filename+" - ERROR")
+        return "ERROR"
+
+#return True if a file excists and False if not
+def isFile(object):
+    try:
+        os.listdir(object)
+        return False
+    except Exception:
+        return True
+
+#creates a treeview of a directory with the filehshes
+def createHashtree(directory, algorythm='sha1'):
+    if platform.system() == 'Linux':
+        slash = '/'
+    elif platform.system() == 'Windows':
+        slash = '\\'
+    directory = directory + slash
+    checksum = ''
+    jsonstring = '{'
+    objects = os.listdir(directory)
+    for i in range(0, len(objects)):
+        filename = directory+objects[i]
+        if isFile(filename):
+            checksum = genFileChecksum(filename, algorythm)
+            jsonstring = jsonstring + '"' + objects[i] + '":"' + str(checksum) + '",'
+        else:
+            if platform.system() == 'Linux':
+                slash = '/'
+            elif platform.system() == 'Windows':
+                slash = '\\'
+            jsonstring = jsonstring + '"' + objects[i] + '":' + createHashtree(directory+objects[i]+slash, algorythm) + ','
+    if jsonstring[-1] =="{":
+        jsonstring = jsonstring + "}"
+    else:
+        jsonstring = jsonstring[:-1] + "}"
+    return  jsonstring
+
+if __name__ == "__main__":
+    directory = os.path.dirname(os.path.realpath(__file__)) #working directory
+    data = createHashtree(directory, "md5")
+    data = json.loads(data)
+    print(json.dumps(data, sort_keys=True, indent=4))
--- src/filehandler.py
+++ src/filehandler.py
@@ -1,57 +1,102 @@
 import os
+import json
 
 #pulls asingle line from a file
-def singlePull(file, line):
-    file = open(file, "r")
-    text = file.readlines()
-    file.close()
-    line = text[line].rstrip()
-    return line
+def read(filename, line=False):
+    try:
+        filename = open(filename, "r")
+        text = filename.readlines()
+        filename.close()
+        if not line:
+            line = text[line].rstrip()
+            return line
+        else:
+            return text
+    except Exception as e:
+        print('ERROR fileHandler.py read(filename,line)\n      ',e)
+
+def readjson(filename):
+    if True:
+        filename = open(filename, "r")
+        output = filename.readlines()
+        text = ""
+        for i in range(0, len(output)):
+            text = text + output[i].replace("\n","")
+        filename.close()
+        return text
+    try:
+        s = 0
+    except Exception as e:
+        print('ERROR fileHandler.py readjson(filename,line)\n      ',e)
 
 #writes a single line into a file (don't have to excist)
-def singlePush(file, line, text):
-    t = []
-    registratedExistance = False
-    while not registratedExistance:
-        try:
-            f = open(file, "r")
-            t = f.readlines()
-            f.close()
-            registratedExistance = True
-            if t[-1] == '':
-                t.pop(len(t) - 1)
-        except Exception:
-            f = open(file, "w")
-            f.close()
-    if len(t) < line:
-        for count in range(len(t), line):
-            t.append('\n')
-    t[line - 1] = text+'\n'
+def write(filename, line, text):
     try:
-        os.remove(file)
-    except Exception:
-        something = 0
-    f = open(file, "w")
-    for count in range(0, len(t)):
-        f.write(t[count])
-    f.close()
+        t = []
+        registratedExistance = False
+        while not registratedExistance:
+            try:
+                f = open(filename, "r")
+                t = f.readlines()
+                f.close()
+                registratedExistance = True
+                if t[-1] == '':
+                    t.pop(len(t) - 1)
+            except Exception:
+                f = open(filename, "w")
+                f.close()
+        if len(t) < line:
+            for count in range(len(t), line):
+                t.append('\n')
+        t[line - 1] = text+'\n'
+        try:
+            os.remove(filename)
+        except Exception:
+            something = 0
+        f = open(filename, "w")
+        for count in range(0, len(t)):
+            f.write(t[count])
+        f.close()
+    except Exception as e:
+        print('ERROR fileHandler.py write(filename,line,text)\n      ',e)
+
+def writejson(filename, data):
+    #print("data to write by fileHandler.writejson" + str(data))
+    with open(filename, "w") as jsonFile:
+        json.dump(data, jsonFile, sort_keys=True, indent=2)
 
 #returns the amount of Lines of a file
-def getLenght(file):
-    t = []
-    f = open(file, "r")
-    t = f.readlines()
-    f.close()
-    return len(t)
+def getLines(filename):
+    try:
+        t = []
+        f = open(filename, "r")
+        t = f.readlines()
+        f.close()
+        return len(t)
+    except Exception as e:
+        print('ERROR fileHandler.py getLines(filename)\n      ',e)
 
 #deletes a file
-def delete(file):
+def delete(filename):
     try:
-        os.remove(file)
+        try:
+            os.remove(filename)
+        except Exception as e:
+            pass
     except Exception as e:
-        something = 0
+        print('ERROR fileHandler.py delete(filename)\n      ', e)
 
 #creates a file
-def create(file):
-    f = open(file, "w")
-    f.close()
+def create(filename, data=False):
+    try:
+        if not data:
+            f = open(filename, "w")
+            f.close()
+        elif str(type(data)) == "<class 'list'>":
+            with open(filename,'wb') as f:
+                f.write(b''.join(data))
+        else:
+            e = "CustomError: Parameters con only be 'False' or bytelist."
+            print('ERROR fileHandler.py create(filename)\n      ', e)
+    except Exception as e:
+        print('ERROR fileHandler.py create(filename)\n      ',e)
