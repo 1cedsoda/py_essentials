11f827e72adff402d9dbcd4dbd4ad0c86e8e3df0
phyyyl <internetz@eclipso.de>
2018-02-11 01:32:32 +0100

    changes version

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # python-essentails
 
-[![Codacy Badge](https://api.codacy.com/project/badge/Grade/fb8bedc284324df8bbcb5f1a7d98f3f9)](https://www.codacy.com/app/phyyyl/py-essentails?utm_source=github.com&utm_medium=referral&utm_content=phyyyl/py-essentails&utm_campaign=badger)
-
 A collection of simple and useful python algorythms and functions.
 
 # File handling (filehandler.py)
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'py_essentails',
   packages = ['py_essentails'],
-  version = '1.4.2',
+  version = '1.4.3',
   description = 'A collection of simple and useful python algorythms and functions.',
   author = 'Phyyyl',
   author_email = 'internetz@eclipso.de',
   url = 'https://github.com/phyyyl/py_essentails',
-  download_url = 'https://github.com/phyyyl/py-essentails/archive/1.4.2.tar.gz',
+  download_url = 'https://github.com/phyyyl/py_essentails/archive/1.4.3.tar.gz',
   keywords = ['py-essentails', 'essentails', 'file', 'random', 'hash', 'simple', 'advancedlist', 'advanced', 'list', 'pretty', 'printing', 'prettyprinting', 'random', 'string', 'hashtree'],
   classifiers = [],
   install_requires=[]
