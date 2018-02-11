1b69d8178006aaeb653b39508fd4734aa85a5d22
phyyyl <internetz@eclipso.de>
2018-02-11 01:26:23 +0100

    changed name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'py-essentails',
-  packages = ['py-essentails'],
-  version = '1.4.1',
+  name = 'py_essentails',
+  packages = ['py_essentails'],
+  version = '1.4.2',
   description = 'A collection of simple and useful python algorythms and functions.',
   author = 'Phyyyl',
   author_email = 'internetz@eclipso.de',
-  url = 'https://github.com/phyyyl/py-essentails',
-  download_url = 'https://github.com/phyyyl/py-essentails/archive/1.4.1.tar.gz',
+  url = 'https://github.com/phyyyl/py_essentails',
+  download_url = 'https://github.com/phyyyl/py-essentails/archive/1.4.2.tar.gz',
   keywords = ['py-essentails', 'essentails', 'file', 'random', 'hash', 'simple', 'advancedlist', 'advanced', 'list', 'pretty', 'printing', 'prettyprinting', 'random', 'string', 'hashtree'],
   classifiers = [],
   install_requires=[]
