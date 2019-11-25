6d43d3e2d34c6e52754e7b272fe47650daef30af
kan <erkan.dem@pm.me>
2019-11-25 03:55:44 +0100

    import from local package

--- py_essentials/__init__.py
+++ py_essentials/__init__.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import py_essentials.xcptns
-import py_essentials.checkup
-checkup.checkup()
-
-import py_essentials.advancedList
-import py_essentials.prettyPrinting
-import py_essentials.fileHandler
-import py_essentials.hashing
-import py_essentials.simpleRandom
+from . import xcptns
+from . import checkup
+from . import advancedList
+from . import prettyPrinting
+from . import fileHandler
+from . import hashing
+from . import simpleRandom
